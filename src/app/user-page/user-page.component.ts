87e8f000facf4d12eaf33a35ab81ea2dda59c658
fcojgodoy <hola@franciscogodoy.es>
2022-01-23 11:22:53 +0100

    feat: user-form validator

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { FormControlStatus } from '@angular/forms';
 import { User } from '../data/models/user';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.scss']
 })
-export class UserPageComponent {
+export class UserPageComponent implements AfterViewInit {
+
+  @ViewChild(UserFormComponent)
+  userFormComponent!: UserFormComponent
+
+  usersFormValid = true;
 
   users: User[] = [
     { id: 1, name: 'Vanesa' },
@@ -15,4 +22,15 @@ export class UserPageComponent {
     { id: 4, name: 'Laura' },
     { id: 5, name: 'Ramiro' }
   ];
+
+  ngAfterViewInit(): void {
+    this.listenUsersFormStatusChangesAndSetProp();
+  }
+
+  listenUsersFormStatusChangesAndSetProp() {
+    this.userFormComponent.usersForm.statusChanges
+    .subscribe((_status: FormControlStatus) => {      
+      this.usersFormValid = this.userFormComponent.usersForm.valid;
+    })
+  }
 }
